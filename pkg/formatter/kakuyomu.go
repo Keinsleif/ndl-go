cba153e610602e0b72eb132dfa456073e88201ac
kazuto28 <mountaindull@gmail.com>
2023-05-15 11:53:51 +0900

    [add] KakuyomuNF 仮実装

--- pkg/formatter/kakuyomu.go
+++ pkg/formatter/kakuyomu.go
@@ -1,7 +1,10 @@
 package formatter
 
 import (
+	"bytes"
 	"embed"
+	"encoding/json"
+	"text/template"
 	ndl "github.com/kazuto28/ndl-go/pkg/downloader"
 )
 
@@ -12,8 +15,39 @@ var themes embed.FS
 var static embed.FS
 
 //go:embed themes/kakuyomu/config.json
-var config []byte
+var configByte []byte
+
+type configJson struct {
+	Medias []string `json:"medias"`
+	Loads  struct {
+		JS  []string `json:"js"`
+		CSS map[string][]string `json:"css"`
+	} `json:"loads"`
+}
 
 func KakuyomuNF(nd *ndl.NovelData) *FormattedNovel {
-	return &FormattedNovel{Static:static}
+	fn := FormattedNovel{Static:static}
+	fn.Episodes = make(map[int]string,len(nd.Novels))
+	tmpl, err := template.ParseFS(themes,"*.html")
+	tmpl = tmpl.Funcs(template.FuncMap{
+		"add":func(a,b int)int{return a+b},
+		"iter":func(a int)[]int{return make([]int, a)},
+	})
+	if err != nil {
+		panic(err)
+	}
+	var conf configJson
+	if err := json.Unmarshal(configByte,conf);err!=nil {
+		panic(err)
+	}
+	var w bytes.Buffer
+	tmpl.ExecuteTemplate(&w,"index",nd)
+	fn.Index = w.String()
+	for part,_ := range nd.Novels {
+		var w bytes.Buffer
+		tmpl.ExecuteTemplate(&w,"base",map[string]any{"nd":nd,"part":part,"config":conf})
+		fn.Episodes[part]= w.String()
+	}
+	return &fn
 }
+

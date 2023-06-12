b0770067fa83e8a33d1a2abc81a347d2ece156f7
kazuto28 <mountaindull@gmail.com>
2023-06-12 09:57:08 +0900

    [add] pkg/cmd/ndl.go func

--- pkg/cmd/ndl/ndl.go
+++ pkg/cmd/ndl/ndl.go
@@ -1,1 +1,39 @@
 package ndl
+
+import (
+	// "context" //TODO
+	"github.com/Keinsleif/ndl-go/pkg/downloader"
+	"github.com/Keinsleif/ndl-go/pkg/env"
+	"github.com/Keinsleif/ndl-go/pkg/errors"
+	"github.com/Keinsleif/ndl-go/pkg/formatter"
+)
+
+
+func NovelDownloader()error{
+	// ctx := context.Background() //TODO
+	e, err := env.MkEnv()
+	if err!=nil{
+		return errors.Wrap(err, "Main", "ERROR")
+	}
+
+	for e.Src.HasNext {
+		nd, err := downloader.GetND(e.Src.Current,*e)
+		if err != nil {
+			return errors.Wrap(err,"Main","ERROR")
+		}
+		err = nd.IE()
+		if err != nil {
+			return errors.Wrap(err,"Main","ERROR")
+		}
+		err = nd.NE()
+		if err != nil {
+			return errors.Wrap(err,"Main","ERROR")
+		}
+		err = formatter.NF(nd.Data(),e)
+		if err != nil {
+			return errors.Wrap(err,"Main","ERROR")
+		}
+		e.Src.Next()
+	}
+	return nil
+}

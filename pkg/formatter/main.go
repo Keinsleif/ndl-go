ba7bf087159d99a3f0dc6e57c433bc69adc8fefb
kazuto28 <mountaindull@gmail.com>
2023-06-12 09:56:18 +0900

    [update] formatter wrap error

--- pkg/formatter/main.go
+++ pkg/formatter/main.go
@@ -10,8 +10,10 @@ import (
 	"text/template"
 	"encoding/json"
 	"github.com/Keinsleif/ndl-go/pkg/env"
+	"github.com/Keinsleif/ndl-go/pkg/errors"
 	"github.com/Keinsleif/ndl-go/pkg/util"
 	ndl "github.com/Keinsleif/ndl-go/pkg/downloader"
+	"golang.org/x/exp/slices"
 )
 
 //go:embed themes/*
@@ -29,15 +31,24 @@ func NewNameFormatter(ni *ndl.NovelInfo,e *env.Env) *strings.Replacer{
 	return strings.NewReplacer("{title}",ni.Title,"{theme}",e.Theme)
 }
 
-func GenericNF(nd *ndl.NovelData,e *env.Env) error{
+func NF(nd *ndl.NovelData,e *env.Env) error{
+	var genericThemes = []string{"kakuyomu","narou"}
 	if e.Theme == "auto"{
 		e.Theme = nd.Info.Site
 	}
+	if slices.Contains(genericThemes,e.Theme){
+		return GenericNF(nd,e)
+	}else {
+		return errors.New("NovelFormatter","Theme is unsupported.","ERROR")
+	}
+}
+
+func GenericNF(nd *ndl.NovelData,e *env.Env) error{
 	repl := NewNameFormatter(nd.Info,e)
 	destDir := filepath.Join(repl.Replace(e.OutPath), repl.Replace(e.OutFormat))
 	err := os.MkdirAll(destDir,os.ModePerm)
 	if err!=nil{
-		return err
+		return errors.Wrap(err,"GenericNF","ERROR")
 	}
 	tmpl := template.New("").Funcs(template.FuncMap{
 		"add":func(a,b int)int{return a+b},
@@ -58,27 +69,27 @@ func GenericNF(nd *ndl.NovelData,e *env.Env) error{
 	}
 	w, err := os.Create(filepath.Join(destDir,"index.html"))
 	if err!=nil {
-		return err
+		return errors.Wrap(err,"GenericNF","ERROR")
 	}
 	err = tmpl.ExecuteTemplate(w,"index.html",map[string]any{"nd":nd,"config":conf})
 	if err != nil {
-		return err
+		return errors.Wrap(err,"GenericNF","ERROR")
 	}
 	w.Close()
 	for part := range nd.Novels {
 		w, err := os.Create(filepath.Join(destDir,strconv.Itoa(part)+".html"))
 		if err!=nil {
-			return err
+			return errors.Wrap(err,"GenericNF","ERROR")
 		}
 		err = tmpl.ExecuteTemplate(w,"base.html",map[string]any{"nd":nd,"part":part,"config":conf})
 		if err != nil {
-			return err
+			return errors.Wrap(err,"GenericNF","ERROR")
 		}
 		w.Close()
 	}
 	err = util.CopyEmbedDir(path.Join("themes",e.Theme,"static"),filepath.Join(destDir,"static"),themes)
 	if err!=nil{
-		return err
+		return errors.Wrap(err,"GenericNF","ERROR")
 	}
 	return nil
 }

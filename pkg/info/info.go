24714091dcb552c6426f6a69562fa1386db172eb
kazuto28 <mountaindull@gmail.com>
2023-06-26 16:39:56 +0900

    [add] VERSION

--- pkg/info/info.go
+++ pkg/info/info.go
@@ -6,6 +6,7 @@ const URL = "https://github.com/Keinsleif/ndl-go/"
 
 const APPNAME = "NDL-GO"
 const DESCRIPTION = "Novel downloader written in GO"
+const VERSION = "1.0.0"
 const LICENSE = "MIT License"
 
 // APPFAM is the group of app that use same config dir & file

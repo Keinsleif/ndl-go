80a58fb10c78e8032ec1a346f42e483a129e3c61
kazuto28 <mountaindull@gmail.com>
2023-06-26 15:27:47 +0900

    [add] ndl-go cmd function

--- cmd/ndl-go/ndl-go.go
+++ cmd/ndl-go/ndl-go.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"os"
+	"fmt"
+	"github.com/Keinsleif/ndl-go/pkg/cmd/ndl"
+)
+
 func main() {
-	
+	err := ndl.NovelDownloader()
+	if err != nil {
+		fmt.Fprintln(os.Stderr,err.Error())
+	}
 }
